aab9ba898c9cb69a4186a687e1cc8a2739295e49
juan-Cai <juand-caicedogu@unilibre.edu.co>
2025-03-11 18:38:03 +0000

    as

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAZoUrWSDya231_UAxbAZdlZZgpvJGopNA",
+    authDomain: "galeria-6dbea.firebaseapp.com",
+    databaseURL: "https://galeria-6dbea-default-rtdb.firebaseio.com",
+    projectId: "galeria-6dbea",
+    storageBucket: "galeria-6dbea.firebasestorage.app",
+    messagingSenderId: "528628894235",
+    appId: "1:528628894235:web:b680f7dff3ae04139846be",
+    measurementId: "G-WDWMKH84J5"
+  }
 };
